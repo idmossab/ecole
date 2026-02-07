c028095726d163c86debc9ab1217e57089d3d2ab
idmossab <idmossab@gmail.com>
2026-02-07 13:03:37 +0000

    fixed button back in manage studies

--- frontend/src/app/pages/manage-studies/manage-studies.component.ts
+++ frontend/src/app/pages/manage-studies/manage-studies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 import { ApiService, AdminCourse, AdminCertificate } from '../../core/api.service';
 import { Media } from '../../core/models';
@@ -8,7 +9,7 @@ import { Media } from '../../core/models';
 @Component({
   selector: 'app-manage-studies',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './manage-studies.component.html',
   styleUrl: './manage-studies.component.css'
 })
